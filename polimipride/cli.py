0759cdfe53fa2358248cc9fdeecc80d97ccc1d60
Davide Depau <davide@depau.eu>
2018-03-19 00:07:17 +0100

    Optionally provide frozen destionation as command line argument (for Travis CI)

--- polimipride/cli.py
+++ polimipride/cli.py
@@ -1,6 +1,12 @@
+import click
+
 from . import app, freezer
 
+
 @app.cli.command()
-def freeze():
+@click.option('--dest', '-d', type=str, default=None)
+def freeze(dest):
     """Generate static website."""
+    if dest:
+        app.config["FREEZER_DESTINATION"] = dest
     freezer.freeze()
