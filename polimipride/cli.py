cb6fa64c6e899cc42188965fdac78f182e8d66bf
Davide Depau <davide@depau.eu>
2018-03-19 00:12:39 +0100

    Add help text for freeze command

--- polimipride/cli.py
+++ polimipride/cli.py
@@ -4,7 +4,7 @@ from . import app, freezer
 
 
 @app.cli.command()
-@click.option('--dest', '-d', type=str, default=None)
+@click.option('--dest', '-d', type=str, default=None, help="Path where frozen site will be placed.")
 def freeze(dest):
     """Generate static website."""
     if dest:
